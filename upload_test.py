247f4fbdd845b60d0351e4269b1b63822cc86d89
gabcn <gabrielcn@id.uff.br>
2024-07-22 17:11:45 -0300

    test upload instructions

---
+++ upload_test.py
@@ -0,0 +1,20 @@
+import os
+
+
+# testing
+# Testing
+# * Upload:
+# ```
+# py -m build
+# py -m twine upload --repository testpypi dist/*
+# ```
+
+# * Install the package (test):
+# ```
+# py -m pip install --index-url https://test.pypi.org/simple --no-deps NsgOrcFx_nsgeng
+# ```
+# If already installed:
+# ```
+# py -m pip uninstall NsgOrcFx_nsgeng
+# py -m pip install --upgrade --index-url https://test.pypi.org/simple --no-deps NsgOrcFx_nsgeng
+# ```
